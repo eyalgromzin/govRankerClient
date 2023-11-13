a50b62fa98416b5578556933ac0c6daea6fa81bd
eyalgromzin <eyalgromzin@gmail.com>
2023-11-13 13:21:52 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import Cart from "./components/Cart";
-import Products from "./components/Products";
-import { useEffect } from "react";
-import Add from "./components/Add";
-import List from "./components/List";
-import { fetchPerson } from "./store/features/personSlice";
-import { useAppDispatch } from "./store/store";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, incrementByAmount } from "./redux/counterSlice";
+import { RootState } from "./redux/store";
+import axios from 'axios';
 
-function App() {
-  // const dispatch = useAppDispatch();
-  // useEffect(() => {
-  //   dispatch(fetchPerson());
-  // });
+export default function App() {
+  const { count } = useSelector((state: RootState) => state.counter1); // see store.ts
+  const dispatch = useDispatch();
+  
+  const getResultFromServer = async () => {
+    console.log('in getResultFromServer()')
+    const res = axios.get('http://localhost:3000/test2').then(res => {
+      console.log('in res')
+      let asd = 4
+      asd += 1
+      console.log(res)
+    })
+
+    
+  }
+
   return (
-    <div>
-      <Add />
-      <List />
-      <Products />
-      <Cart />
+    <div className="App">
+      <button onClick={() => getResultFromServer()}>
+        call server
+      </button>
+
+      <br />
+      <br />
+      <br />
+
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        ++++
+      </button>
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        -----
+      </button>
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          dispatch(incrementByAmount(10));
+        }}
+      >
+        +10
+      </button>
     </div>
   );
 }
-
-export default App;
