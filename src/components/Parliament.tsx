513d8b4ad63c47c489ce9cc0e2bc673a5a8f6e9f
Yuli <yulikga@gmail.com>
2023-12-06 21:04:49 +0200

    add css and imgs

--- src/components/Parliament.tsx
+++ src/components/Parliament.tsx
@@ -12,15 +12,16 @@ interface MyObject {
 // @ts-ignore
 const ParliamentComp = ({ onPartyMemberSelect }) => {
     const myMembers: MyObject[] = [
-        {id:1, name:'Bibi', description: 'Bla Bla Bla',url:'/src/assets/images/b.jpg' },
-        {id:2, name:'Bibi', description: 'Bla Bla Bla',url:'/src/assets/images/b.jpg' },
-        {id:3, name:'Bibi', description: 'Bla Bla Bla',url:'/src/assets/images/b.jpg' },
-        {id:4, name:'Bibi', description: 'Bla Bla Bla',url:'/src/assets/images/b.jpg' },
-        {id:5, name:'Bibi', description: 'Bla Bla Bla',url:'/src/assets/images/b.jpg' },
-        {id:6, name:'Bibi', description: 'Bla Bla Bla',url:'/src/assets/images/b.jpg' },
-        {id:7, name:'Bibi', description: 'Bla Bla Bla',url:'/src/assets/images/b.jpg' },
-        {id:8, name:'Bibi', description: 'Bla Bla Bla',url:'/src/assets/images/b.jpg' },
-        {id:9, name:'Bibi', description: 'Bla Bla Bla',url:'/src/assets/images/b.jpg' }
+        {id:1, name:'ליכוד', description: 'ליכוד',url:'/src/assets/images/b.jpg' },
+        {id:2, name:'יש עתיד', description:'יש עתיד',url:'/src/assets/images/lapid.jpg' },
+        {id:3, name:'הציונות הדתית', description: 'הציונות הדתית',url:'/src/assets/images/sm.jpeg' },
+        {id:4, name:'המחנה הממלחתי', description: 'המחנה הממלחתי',url:'/src/assets/images/ganz.jpeg' },
+        {id:5, name:'ש"ס', description: 'ש"ס',url:'/src/assets/images/shas.jpg' },
+        {id:6, name:'יהדות התורה', description: 'יהדות התורה',url:'/src/assets/images/ya.jpg' },
+        {id:7, name:'ישראל ביתנו', description: 'ישראל ביתנו',url:'/src/assets/images/lib.jpeg' },
+        {id:8, name:'העבודה', description: 'העבודה',url:'/src/assets/images/micha.jpg' },
+        {id:9, name:'מרצ', description: 'מרצ',url:'/src/assets/images/gor.jpg' },
+        {id:10, name:'רע"ם', description: 'רע"ם',url:'/src/assets/images/ram.jpg' }
     ];
     // הפעולה הפנימית שמפעילה את הפונקציה המועברת
     const handleMemberSelect = () => {
@@ -34,7 +35,7 @@ const ParliamentComp = ({ onPartyMemberSelect }) => {
         <div>
             <div className="cool-header">
                 <h1 className="cool-text">
-                    <span className="blue-text">ראשי</span>
+                    <span className="blue-text">ראשי </span>
                     <span className="gray-text">מפלגות</span>
                 </h1>
             </div>
